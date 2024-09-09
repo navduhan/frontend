a2101f72fd5f8c269c3f8d6a01aecc45a1e70a61
navduhan <duhan27dec@gmail.com>
2024-09-09 12:27:50 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import './App.css';
+import './App.scss';
 import "antd/dist/antd.min.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React  from 'react';
 
 import {MINPRED} from './MINPRED';
